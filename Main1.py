c443e75a5ea5e0692aafc96a01ed03c93fd1a2da
vipulvanne3-23 <vipulvanne3@gmail.com>
2026-02-24 09:10:30 +0530

    Update Main1.py

--- Main1.py
+++ Main1.py
@@ -1,7 +1,8 @@
-def Add(a,b):
-  return a+b
-  def Display ():
+def Add(a, b):
+    return a + b
+
+def Display():
     print("Hello")
 
-if__name__=='__main__':
-Display()
+if __name__ == '__main__':
+    Display()
